e89da129e74493b9ca1d99543a2bcc508366440c
patodiayogesh <yogesh.patodia@columbia.edu>
2022-10-25 00:18:24 -0400

    validation after training

--- main.py
+++ main.py
@@ -47,8 +47,8 @@ class BaselineTrainer:
             # accumulate_grad_batches=12,
             # strategy='ddp',
             # Validation between epoch
-            # limit_val_batches=0.0,
-            # check_val_every_n_epoch=1
+            limit_val_batches=0.0,
+            check_val_every_n_epoch=1
         )
 
     def train_model(self):

3c1abc03260962936d1cd65faed38ddfc503808d
patodiayogesh <yogesh.patodia@columbia.edu>
2022-10-26 12:22:11 -0400

    ckpt to bin

---
+++ ckpt_to_bin.py
@@ -0,0 +1,8 @@
+from model import BaselineModel
+
+if __name__ == '__main__':
+
+    path = 'lightning_logs/gpt/checkpoints/'
+    model = BaselineModel.load_from_checkpoint(path + 'epoch=5-step=7632.ckpt')
+    model.model.save_pretrained(path)
+

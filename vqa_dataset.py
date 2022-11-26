fee7cad55ee30dbb70d0a916273870da2465b743
Mukesh-BR <mukeshbr.br309@gmail.com>
2022-11-25 19:39:31 -0500

    VQA Dataset

--- vqa_dataset.py
+++ vqa_dataset.py
@@ -9,6 +9,59 @@ from utils import calculate_number_of_mask_tokens
 from masking_stratergies import epoch_aware_mask, text_infilling
 import json
 
+class VQATestDataset(Dataset):
+
+    def __init__(self,
+                 questions_file,
+                 transform=None,
+                 ):
+
+        self.image_dir = 'datasets/vqa_images/test/'
+        self.pairs = self.load_dataset(questions_file)
+    
+    def load_dataset(self,question_json):
+      f = open(question_json)
+      questions = json.load(f)
+
+      questions_dict = {}
+      images = {}
+      grand_dict = []
+
+      for val in questions['questions']:
+        image_id = val['image_id']
+        question = val['question']
+        question_id = val['question_id']
+
+        image_name = 'COCO_train2014_'+'0'*(12-len(str(image_id)))+str(image_id)+'.jpg'
+        questions_dict[question_id] = {'Question':question}
+        if not image_name in images:
+          images[image_name] = []
+        images[image_name].append(question_id)
+      
+      for v in images:
+        image_name = v
+        question_ids = images[v]
+        for q in question_ids:
+          obj = questions_dict[q]
+          q = obj['Question']
+          grand_dict.append([image_name,q,' '])
+      
+      return grand_dict
+
+    def __len__(self):
+
+        return len(self.pairs)
+
+    def __getitem__(self, index):
+
+        question = self.pairs[index][1]
+        answer = self.pairs[index][2]
+        image_filename = self.pairs[index][0]
+        img = Image.open(self.image_dir + image_filename)
+        if self.transform:
+            img = self.transform(img)
+        return img, question,answer, image_filename
+
 class VQAPredictionDataset(Dataset):
 
     def __init__(self,
@@ -17,7 +70,7 @@ class VQAPredictionDataset(Dataset):
                  transform=None,
                  ):
 
-        self.image_dir = 'datasets/vqa_images/'
+        self.image_dir = 'datasets/vqa_images/val/'
         self.pairs = self.load_dataset(questions_file,answers_file)
     
     def load_dataset(self,question_json,answer_json):
@@ -76,10 +129,11 @@ class VQADataset(Dataset):
     def __init__(self,
                  questions_file,
                  answers_file,
+                 split='train',
                  transform=None,
                  ):
 
-        self.image_dir = 'datasets/vqa_images/'
+        self.image_dir = 'datasets/vqa_images/'+split+'/'
         self.pairs = self.load_dataset(questions_file,answers_file)
     
     def load_dataset(self,question_json,answer_json):
@@ -180,17 +234,17 @@ class VQADatasetModule(pl.LightningDataModule):
     def setup(self, stage=None):
 
         if stage == 'fit' or stage is None:
-            self.train_dataset = VQADataset(self.train_questions,self.train_answers, self.transform)
-            self.val_dataset = VQADataset(self.val_questions,self.val_answers, self.transform)
+            self.train_dataset = VQADataset(self.train_questions,self.train_answers,'train', self.transform)
+            self.val_dataset = VQADataset(self.val_questions,self.val_answers,'val', self.transform)
 
         if stage == 'validate':
-            self.val_dataset = VQADataset(self.val_questions,self.val_answers, self.transform)
+            self.val_dataset = VQADataset(self.val_questions,self.val_answers,'val', self.transform)
 
         if stage == 'test':
-            self.val_dataset = VQADataset(self.val_questions,self.val_answers, self.transform)
+            self.val_dataset = VQADataset(self.val_questions,self.val_answers,'val', self.transform)
 
         if stage == 'predict':
-            self.val_dataset = VQAPredictionDataset(self.val_questions,self.val_answers, self.transform)
+            self.val_dataset = VQAPredictionDataset(self.val_questions,self.val_answers,'val', self.transform)
 
     def _set_image_feature_extractor(self, image_feature_extractor):
         self.image_feature_extractor = image_feature_extractor
@@ -226,23 +280,7 @@ class VQADatasetModule(pl.LightningDataModule):
             truncation=True,
             return_tensors="pt",
         )
-        if self.multi_modal:
-            # if self.mask:
-            #     if self.mask == 'empty':
-            #         input_text = ['' for _ in batch_data]
-            #     elif self.mask == 'epoch_aware_mask':
-            #         input_text = [epoch_aware_mask(self.epoch, x) for x in captions]
-            #     elif self.mask == 'text_infilling':
-            #         input_text = [text_infilling(x) for x in captions]
-            #     input_text_encodings = self.tokenizer(input_text,
-            #                                           padding='longest',
-            #                                           return_tensors='pt')
-            # else:
-            #     input_text_encodings = caption_encodings
-            # labels = caption_encodings
-            return image_encodings, question_encodings, answer_encodings
-        else:
-            return image_encodings, question_encodings
+        return image_encodings, question_encodings, answer_encodings
 
     def prediction_tokenization(self, batch_data):
 
@@ -253,28 +291,14 @@ class VQADatasetModule(pl.LightningDataModule):
         '''
 
         image_tensors = [t[0] for t in batch_data]
-        captions = [t[1] for t in batch_data]
-        image_filenames = [t[2] for t in batch_data]
+        questions = [t[1] for t in batch_data]
+        answers = [t[2] for t in batch_data]
+        image_filenames = [t[3] for t in batch_data]
 
         image_encodings = self.image_feature_extractor(image_tensors, return_tensors='pt').pixel_values
-        if self.multi_modal:
-            if self.mask == 'empty':
-                input_text = ['' for _ in batch_data]
-                input_text_encodings = self.tokenizer(input_text,
-                                                      return_tensors='pt')
-            elif self.mask == 'epoch_aware_mask':
-                input_text = [' '.join(['<mask>']*self.median_length) for _ in batch_data]
-                input_text_encodings = self.tokenizer(input_text,
-                                                      return_tensors='pt')
-            
-            elif self.mask == 'text_infilling':
-                input_text = ['<mask>' for _ in batch_data]
-                input_text_encodings = self.tokenizer(input_text,
-                                                      return_tensors='pt')
+        question_encodings = self.tokenizer(questions,return_tensors='pt')
 
-            return image_encodings, input_text_encodings, captions, image_filenames
-        else:
-            return image_encodings, captions, image_filenames
+        return image_encodings, question_encodings, answers, image_filenames
 
     def train_dataloader(self):
         return torch.utils.data.DataLoader(
